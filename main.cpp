2742d33624fd9064950b2e27f50f0216cdbf0e2f
Alexey Mukovnin <alexey.1983@gmail.com>
2024-01-27 14:38:03 +0300

    use control via stdin

--- main.cpp
+++ main.cpp
@@ -1,26 +1,50 @@
 #include "args.hpp"
 #include "event.hpp"
-#include "log.hpp"
+#include "input.hpp"
 #include "output.hpp"
 #include "tracer.hpp"
 #include <cstdlib>
 #include <memory>
+#include <pthread.h>
 #include <unistd.h>
 
 int main(int argc, char **argv) {
   ArgsParser args(argc, argv);
   if (!args)
     return EXIT_FAILURE;
+  std::unique_ptr<Input> input;
   std::unique_ptr<Output> output;
   const char *file = args.outputFile();
-  if (file)
+  pthread_t mainThread = pthread_self();
+  auto inCallback = [&](Command cmd, unsigned arg) {
+    switch (cmd) {
+    case Command::Quit:
+      pthread_kill(mainThread, SIGTERM);
+      break;
+    case Command::SortingOrder:
+      output->toggleSortingOrder();
+      break;
+    case Command::SortingColumn:
+      output->setSorting(static_cast<Column>(arg));
+      break;
+    case Command::Up:
+      dynamic_cast<TerminalOutput *>(output.get())->lineUp();
+      break;
+    case Command::Down:
+      dynamic_cast<TerminalOutput *>(output.get())->lineDown();
+      break;
+    }
+  };
+  auto outCallback = [&](const EventInfo &ei) { output->handleEvent(ei); };
+  if (file) {
     output.reset(new FileOutput(file));
-  else
+  } else {
     output.reset(new TerminalOutput);
+    input.reset(new Input(inCallback));
+  }
   output->setSorting(args.sortType());
   if (args.reverseSorting())
     output->toggleSortingOrder();
-  auto outCallback = [&](const EventInfo &ei) { output->handleEvent(ei); };
   Tracer tracer = args.traceeArgs() ? Tracer(args.traceeArgs(), outCallback)
                                     : Tracer(args.traceePid(), outCallback);
   output->setProcessInfo(tracer.traceePid(), tracer.traceeCmdLine());
